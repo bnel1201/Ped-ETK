4fb679f237a8d8a0b9c328fcd3f175654930ed99
brandon.nelson <brandon.nelson@scl.fda.gov>
2022-09-22 10:33:14 -0400

    combined processed and baseline mtf curve plotting into a single file

--- evaluation/MTF/_2_generate_MTF_plots.sh
+++ evaluation/MTF/_2_generate_MTF_plots.sh
@@ -1,9 +1,16 @@
-python plot_mtf_curves.py 
+# plot fbp baseline mtf
+datadir=/gpfs_projects/brandon.nelson/DLIR_Ped_Generalizability/geomtric_phantom_studies/CTP404/monochromatic/
+output_fname=fbp_mtf_baseline.png
+python plot_mtf_curves.py $datadir $output_fname
 
-python plot_proc_mtf_curves.py 
+# plot redcnn mtf
+output_fname=mtf_redcnn.png
+python plot_mtf_curves.py $datadir $output_fname --processed
+
+# python plot_proc_mtf_curves.py 
 
 python plot_mtf_cutoffs.py
 
 python plot_proc_mtf_cutoffs.py 
 
-python plot_diff_mtf_cutoffs.py
+# python plot_diff_mtf_cutoffs.py
--- evaluation/MTF/plot_mtf_curves.py
+++ evaluation/MTF/plot_mtf_curves.py
@@ -1,24 +1,40 @@
 # %%
-import matplotlib.pyplot as plt
+import argparse
 from pathlib import Path
+
+import matplotlib.pyplot as plt
 from utils.mtf_plot import plot_patient_diameter_mtf
 
 
-def main():
-    datadir = Path('/gpfs_projects/brandon.nelson/DLIR_Ped_Generalizability/geomtric_phantom_studies/CTP404/monochromatic/')
-    patient_dirs = sorted(list(datadir.glob('diameter*')))
+def main(datadir=None, output_fname=None, processed=False):
+    datadir = datadir or '/gpfs_projects/brandon.nelson/DLIR_Ped_Generalizability/geomtric_phantom_studies/CTP404/monochromatic/'
+    patient_dirs = sorted(list(Path(datadir).glob('diameter*')))
 
     plt.style.use('seaborn')
     f, axs = plt.subplots(2, 3, figsize=(9, 7), sharex='col', sharey='row',
                         gridspec_kw=dict(hspace=0.1, wspace=0.1))
     for patient_dir, ax in zip(patient_dirs, axs.flatten()):
-        mtf_csv_fname = patient_dir / 'I0_3000000' / 'fbp_sharp_v001_mtf.csv'
+        if processed:
+            mtf_csv_fname = patient_dir / 'I0_3000000_processed' / 'fbp_sharp_v001_mtf.csv'
+        else:
+            mtf_csv_fname = patient_dir / 'I0_3000000' / 'fbp_sharp_v001_mtf.csv'
         plot_patient_diameter_mtf(mtf_csv_fname, ax=ax)
     # %%
-    fname = 'fbp_mtf_baseline.png'
-    f.savefig(fname, dpi=600)
-    print(f'File saved: {fname}')
+    if output_fname is None:
+        plt.show()
+    else:
+        # fname = output_fname or 'fbp_mtf_baseline.png'
+        f.savefig(output_fname, dpi=600)
+        print(f'File saved: {output_fname}')
 
 # %%
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Plots MTF Curves')
+    parser.add_argument('datadir',
+                        help="directory containing different patient diameter CT simulations")
+    parser.add_argument('output_fname',
+                        help="filename for the saved plot")
+    parser.add_argument('--processed', action='store_true', default=False,
+                        help='boolean to plot the processed results, defaults to false using the baseline')
+    args = parser.parse_args()
+    main(args.datadir, output_fname=args.output_fname, processed=args.processed)
