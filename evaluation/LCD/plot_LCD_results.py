94571bb7404179194cf558d3e81ce78b9ca3a8c6
brandon.nelson <brandon.nelson@scl.fda.gov>
2022-11-02 14:41:05 -0400

    precommit before saving adult_ref as h5 file

--- evaluation/LCD/plot_LCD_results.py
+++ evaluation/LCD/plot_LCD_results.py
@@ -8,15 +8,33 @@ import matplotlib.pyplot as plt
 import h5py
 
 
-def load_adult_ref_data(basedir=None, dose_idx=-1):
+def load_adult_dataset(basedir=None, recon='cnn', dose_idx=-1):
     basedir = basedir or Path('/gpfs_projects/prabhat.kc/lowdosect/transfers/transfers_4_spie/exps/quant_analysis/mita_lcd/results/95d_2_75d_2_25d_p96_no_norm/no_norm/redcnn/augTrTaTdT/')
     auc_means = []
     auc_stds = []
+    recon_idx = 1 if recon=='cnn' else 0
     for i in range(4):
         data = scipy.io.loadmat(basedir / f'_idx_{i+1}.mat')
-        auc_means.append(data['auc_all'][:].mean(axis=0)[1, dose_idx]) #get last dose level (highest)
-        auc_stds.append(data['auc_all'][:].std(axis=0)[1, dose_idx]) # according to line 124 of </gpfs_projects/prabhat.kc/lowdosect/transfers/transfers_4_spie/exps/quant_analysis/mita_lcd/mita_lcd_plot_ct_poster.m> 0th index is fbp, 1st is DLIR
+        auc_means.append(data['auc_all'][:].mean(axis=0)[recon_idx, dose_idx]) #get last dose level (highest)
+        auc_stds.append(data['auc_all'][:].std(axis=0)[recon_idx, dose_idx]) # according to line 124 of </gpfs_projects/prabhat.kc/lowdosect/transfers/transfers_4_spie/exps/quant_analysis/mita_lcd/mita_lcd_plot_ct_poster.m> 0th index is fbp, 1st is DLIR
     return auc_means, auc_stds
+
+def load_all_adult_data():
+    basedir = Path("/gpfs_projects/prabhat.kc/lowdosect/transfers/transfers_4_spie/exps/quant_analysis/mita_lcd")
+    resultsdir = basedir / "results/quarter/8p_uni_norm/dncnn/augTrTaTdT/"
+    ndose_levels = 5
+    nrecons = 2
+    nlesions = 4
+    auc_means = np.zeros([ndose_levels, nrecons, nlesions])
+    auc_stds = np.zeros([ndose_levels, nrecons, nlesions])
+    for dose_idx in range(ndose_levels):
+        for recon_idx, recon in enumerate(['fbp', 'cnn']):
+            auc_means[dose_idx, recon_idx, :], auc_stds[dose_idx, recon_idx, :] = load_adult_dataset(resultsdir, dose_idx=dose_idx, recon=recon)
+    return auc_means, auc_stds
+
+    # I should have all the adult data from Prabhat saved as a h5 file so I can easily swap it out for different results, i.e. to compare against Rongping's MITA_LCD.m to make sure everything is consistent
+# %%
+
 
 
 def main(results_dir, output_fname):
@@ -25,7 +43,7 @@ def main(results_dir, output_fname):
     These are reversed from matlab which is F index, but Python is C indexed
     """
 
-    plt.style.use('seaborn')
+    # plt.style.use('seaborn')
 
     # h5file = '/home/brandon.nelson/Dev/DLIR_Ped_Generalizability/geomtric_phantom_studies/results/LCD/LCD_results.h5'
     h5file = f'{results_dir}/LCD_results.h5'
@@ -120,8 +138,14 @@ f.close()
 auc_mean, auc_std = auc.mean(axis=0), auc.std(axis=0)
 snr_mean, snr_std = snr.mean(axis=0), auc.std(axis=0)
 
-adult_auc_means, adult_auc_stds = load_adult_ref_data()
-
+# %% [markdown]
+# save original adult ref data from CT conference poster <>
+# %%
+adult_auc_means, adult_auc_stds = load_all_adult_data()
+adult_ref_h5file = f'{results_dir}/adult_ref_LCD_results.h5'
+with h5py.File(adult_ref_h5file, 'w') as f:
+    dset = f.create_dataset('auc', (10, )) 
+# %%
 dose_idx=0
 recon_idx=recon_types.index('fbp')
 lesion_idxs = [0, 1, 3, 2]
@@ -175,10 +199,14 @@ for diam_idx, d in enumerate(diameters):
         # for d in diam_idx:
         ax.errorbar(dose_levels_pct, auc_mean[:, fbp_idx, lesion_idx, diam_idx], yerr=auc_std[:, fbp_idx, lesion_idx, diam_idx], label=f'FBP {diameters[diam_idx]:0.0f} mm')
         ax.errorbar(dose_levels_pct, auc_mean[:, cnn_idx, lesion_idx, diam_idx], yerr=auc_std[:, cnn_idx, lesion_idx, diam_idx], label=f'REDCNN {diameters[diam_idx]:0.0f} mm')
-        ax.errorbar([100], adult_auc_means[lesion_idx],
-                    yerr=adult_auc_stds[lesion_idx],
-                    fmt='*', markersize=10,
-                    color='black', label='Adult Reference\n(340 mm FOV)')
+        ax.errorbar(dose_levels_pct, adult_auc_means[:, fbp_idx, lesion_idx],
+                    yerr=adult_auc_stds[:, fbp_idx, lesion_idx],
+                    fmt='--', markersize=10,
+                    color='black', label='Adult FBP Reference\n(212 mm FOV)')
+        ax.errorbar(dose_levels_pct, adult_auc_means[:, cnn_idx, lesion_idx],
+                    yerr=adult_auc_stds[:, cnn_idx, lesion_idx],
+                    fmt='--', markersize=10,
+                    color='gray', label='Adult REDCNN Reference\n(212 mm FOV)')
         # ax.plot(dose_levels_pct, auc_mean_diff[:, d], label=f'{diameters[d]:0.0f} mm')
         ax.set_title(f'{lesion_radii_mm[lesion_idx]} mm diameter\n{lesion_HUs[lesion_idx]} HU disk')
         if not subplot_idx % 2:
@@ -190,4 +218,71 @@ for diam_idx, d in enumerate(diameters):
     output_fname = output_dir / f'LCD_v_dose_diameter_{d}mm.png'
     fig.suptitle(f'Patient Diameter: {diameters[diam_idx]:0.0f} mm (FOV: {diameters[diam_idx]*1.1:0.0f} mm)')
     fig.savefig(output_fname, dpi=600) 
+# %% [markdown]
+# experimenting with 2D display
+import pandas as pd
+import seaborn as sns
+
+lesion_HUs = [14, 7, 5, 3]
+lesion_diameter_mm = [3, 5, 7, 10]
+
+def get_auc_df(lesion_idx=0, recon='fbp'):
+    recon_idx = 0 if recon=='fbp' else 1
+    ped_fbp_auc = pd.DataFrame(auc_mean[:, recon_idx, lesion_idx, :], columns = diameters)
+    ped_fbp_auc['Dose Level [%]'] = dose_levels_pct
+    ped_fbp_auc.set_index('Dose Level [%]', inplace=True)
+
+    adult_fbp_auc = pd.DataFrame(adult_auc_means[:, 0, lesion_idx], columns = [200])
+    adult_fbp_auc['Dose Level [%]'] = dose_levels_pct
+    adult_fbp_auc.set_index('Dose Level [%]', inplace=True)
+    auc = ped_fbp_auc.join(adult_fbp_auc)
+    return auc
+
+get_auc_df(0, 'cnn')
+# %%
+import matplotlib.patches as patches
+f, axs = plt.subplots(2, 2, figsize=(12,10),gridspec_kw=dict(hspace=0.4, wspace=0.2))
+for lesion_idx, (ax, lesion_hu, lesion_diam) in enumerate(zip(axs.flatten(), lesion_HUs, lesion_diameter_mm)):
+
+    fbp_auc = get_auc_df(lesion_idx, 'fbp')
+    sns.heatmap(fbp_auc, annot=True, ax=ax, cbar_kws=dict(label=f'AUC'))
+    ax.set_xlabel('Patient Diameter [mm]')
+    twiny = ax.twiny()
+
+    fovs = np.round(fbp_auc.columns*1.1).astype(int).to_list()
+    fovs[fbp_auc.columns.to_list().index(200)] = 340 # From RZ, min FOV for adult scan protocol
+    twiny.set_xticks(ax.get_xticks(), fovs)
+    twiny.set_xlim(ax.get_xlim())
+    twiny.set_xlabel("Recon FOV [mm]")
+    twiny.grid(False)
+    nrows = len(fbp_auc)
+    rect = patches.Rectangle((6, 0.05), 0.97, nrows-0.1, linewidth=3, edgecolor='tab:blue', facecolor='none')
+    ax.annotate("Adult Reference",
+                xy=(6.75, nrows), xycoords='data',
+                xytext=(0.75, 0.025), textcoords='figure fraction',
+                color='tab:blue',
+                arrowprops=dict(facecolor='tab:blue', shrink=0.05), weight='bold')
+    ax.add_patch(rect)
+    ax.set_title(f'{lesion_diam} mm, {lesion_hu} HU')
+f.suptitle("FBP")
+# %%
+f, axs = plt.subplots(2, 2, figsize=(10,10))
+for lesion_idx, (ax, lesion_hu, lesion_diam) in enumerate(zip(axs.flatten(), lesion_HUs, lesion_diameter_mm)):
+    fbp_auc = get_auc_df(lesion_idx, 'cnn')
+    sns.heatmap(fbp_auc, annot=True, ax=ax, cbar_kws=dict(label=f'AUC'))
+    ax.set_xlabel('Patient Diameter [mm]')
+    ax.set_title(f'{lesion_diam} mm, {lesion_hu} HU')
+f.suptitle("REDCNN")
+# %%
+f, axs = plt.subplots(2, 2, figsize=(10,10), sharex=True, sharey=True)
+for lesion_idx, (ax, lesion_hu, lesion_diam) in enumerate(zip(axs.flatten(), lesion_HUs, lesion_diameter_mm)):
+
+    fbp_auc = get_auc_df(lesion_idx, 'fbp')
+    cnn_auc = get_auc_df(lesion_idx, 'cnn')
+
+    sns.heatmap(cnn_auc - fbp_auc, annot=False, ax=ax, cbar_kws=dict(label=f'AUC'))
+    ax.set_xlabel('Patient Diameter [mm]')
+    ax.set_title(f'{lesion_diam} mm, {lesion_hu} HU')
+f.suptitle("REDCNN - FBP")
+
 # %%
